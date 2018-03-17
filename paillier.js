e0fe09767ffcde313f2cac336a1c411e8506a323
Juan Hernández Serrano <jserrano@entel.upc.edu>
2018-03-17 02:40:16 +0100

    with classes

--- paillier.js
+++ paillier.js
@@ -2,108 +2,21 @@
 
 const bignum = require('bignum');
 
-function lcm(a, b) {
-    return a.mul(b).div(a.gcd(b));
-}
-
-function L(a, n) {
-    return a.sub(1).div(n);
-}
-
-function getGenerator(n, n2 = n.pow(2)) {
-    const alpha = bignum.rand(n);
-    const beta = bignum.rand(n);
-    return alpha.mul(n).add(1).mul(beta.powm(n, n2)).mod(n2);
-}
-
-const paillier = {
-    _publicKey: function (bits, n, g) {
-        // bits
-        this.bits = bits;
-        // n
-        this.n = n;
-        // n2 (cached n^2)
-        this.n2 = n.pow(2);
-        this.g = g;
-    },
-    _privateKey: function (lambda, mu, p, q, publicKey) {
-        this.lambda = lambda;
-        this.mu = mu;
-        this.p = p;
-        this.q = q;
-        this.publicKey = publicKey;
-    },
-    getPublicKey: function (bits, n, g, format) {
-        this.bits = bits;
-        switch (format) {
-        case 'dec':
-            this.n = bignum(n);
-            this.g = bignum(g);
-            break;
-        case 'hex':
-            this.n = bignum(n, 16);
-            this.g = bignum(g, 16);
-            break;
-        case 'base64':
-            this.n = bignum.fromBuffer(new Buffer(n, 'base64'));
-            this.g = bignum.fromBuffer(new Buffer(g, 'base64'));
-            break;
-        case 'bignum':
-            this.n = n;
-            this.g = g;
-            break;
-        default: // Big-endian binary Buffer
-            this.n = bignum.fromBuffer(n);
-            this.g = bignum.fromBuffer(g);
-        }
-        return new paillier._publicKey(this.bits, this.n, this.g);
-    },
-    getPrivateKey: function (lambda, mu, p, q, publicKey, format) {
-        switch (format) {
-        case 'dec':
-            this.lambda = bignum(lambda);
-            this.mu = bignum(mu);
-            this.p = bignum(p);
-            this.q = bignum(q);
-            break;
-        case 'hex':
-            this.lambda = bignum(lambda, 16);
-            this.mu = bignum(mu, 16);
-            this.p = bignum(p, 16);
-            this.q = bignum(q, 16);
-            break;
-        case 'base64':
-            this.lambda = bignum.fromBuffer(new Buffer(lambda, 'base64'));
-            this.mu = bignum.fromBuffer(new Buffer(mu, 'base64'));
-            this.p = bignum.fromBuffer(new Buffer(p, 'base64'));
-            this.q = bignum.fromBuffer(new Buffer(q, 'base64'));
-            break;
-        case 'bignum':
-            this.lambda = lambda;
-            this.mu = mu;
-            this.p = p;
-            this.q = q;
-            break;
-        default: // Big-endian binary Buffer
-            this.lambda = bignum.fromBuffer(lambda);
-            this.mu = bignum.fromBuffer(mu);
-            this.p = bignum.fromBuffer(p);
-            this.q = bignum.fromBuffer(q);
-        }
-        this.publicKey = publicKey;
-        return new paillier._privateKey(this.lambda, this.mu, this.p, this.q, this.publicKey);
-    },
-    generateKeys: function (bitlength=2048, simplevariant) {
-        let p, q, n, phi, n2, g, lambda, mu, keys = {};
+module.exports = class Paillier {
+    constructor(privateKey) {
+        this.privateKey = privateKey;
+        this.publicKey = this.privateKey.publicKey;
+    }
+    static generateKeys(bitlength = 2048, simplevariant) {
+        let p, q, n, phi, n2, g, lambda, mu;
         // if p and q are bitlength/2 long, n is then bitlength long
-        this.bitlength = bitlength;
-        console.log('Generating Paillier keys of', this.bitlength, 'bits');
+        console.log('Generating Paillier keys of', bitlength, 'bits');
         do {
-            p = bignum.prime(this.bitlength / 2);
-            q = bignum.prime(this.bitlength / 2);
+            p = bignum.prime(bitlength / 2);
+            q = bignum.prime(bitlength / 2);
             n = p.mul(q);
             phi = p.sub(1).mul(q.sub(1));
-        } while (q.cmp(p) == 0 || n.bitLength() != this.bitlength);
+        } while (q.cmp(p) == 0 || n.bitLength() != bitlength);
 
         n2 = n.pow(2);
 
@@ -120,26 +33,49 @@ const paillier = {
             mu = L(g.powm(lambda, n2), n).invertm(n);
         }
 
-        keys.publicKey = new paillier._publicKey(this.bitlength, n, g);
-        keys.privateKey = new paillier._privateKey(lambda, mu, p, q, keys.publicKey);
-        return keys;
+        const publicKey = new PaillierPublicKey(this.bitlength, n, g);
+        return new Paillier(new PaillierPrivateKey(lambda, mu, p, q, publicKey));
     }
 };
 
-paillier._publicKey.prototype = {
-    encrypt: function (m) {
-        var r;
+class PaillierPrivateKey {
+    constructor(lambda, mu, p, q, publicKey) {
+        this.lambda = lambda;
+        this.mu = mu;
+        this._p = p;
+        this._q = q;
+        this.publicKey = publicKey;
+    }
+    decrypt(c) {
+        return L(c.powm(this.lambda, this.publicKey._n2), this.publicKey.n).mul(this.mu).mod(this.publicKey.n);
+    }
+}
+
+class PaillierPublicKey {
+    constructor(bits, n, g) {
+        this.n = n;
+        this._n2 = n.pow(2); // cache n^2
+        this.g = g;
+    }
+    encrypt(m) {
+        let r;
         do {
             r = bignum.rand(this.n);
-        } while (r <= 1);
-        return this.g.powm(m, this.n2).mul(r.powm(this.n, this.n2)).mod(this.n2);
+        } while (r.le(1));
+        return this.g.powm(m, this._n2).mul(r.powm(this.n, this._n2)).mod(this._n2);
     }
-};
+}
 
-paillier._privateKey.prototype = {
-    decrypt: function (c) {
-        return L(c.powm(this.lambda, this.publicKey.n2), this.publicKey.n).mul(this.mu).mod(this.publicKey.n);
-    },
-};
+function lcm(a, b) {
+    return a.mul(b).div(a.gcd(b));
+}
 
-module.exports = paillier;
+function L(a, n) {
+    return a.sub(1).div(n);
+}
+
+function getGenerator(n, n2 = n.pow(2)) {
+    const alpha = bignum.rand(n);
+    const beta = bignum.rand(n);
+    return alpha.mul(n).add(1).mul(beta.powm(n, n2)).mod(n2);
+}
--- test.js
+++ test.js
@@ -5,13 +5,12 @@ const bignum = require('bignum');
 
 const keys = paillier.generateKeys(1024); // Change to at least 2048 bits in production state
 
-console.log('modulus:', keys.publicKey.n.bitLength());
-console.log(keys);
+console.log('modulus of', keys.publicKey.n.bitLength(), 'bits');
 
 console.log('\n\nTesting additive homomorphism\n');
 
 const num1 = 10;
-const num2 = -5;
+const num2 = 4;
 const bn1 = bignum(num1).mod(keys.publicKey.n);
 let bn2 = bignum(num2).mod(keys.publicKey.n);
 while (bn2.lt(0)) bn2 = bn2.add(keys.publicKey.n);  // bug in bignum? mod of negative keeps being negative. This should fix it
@@ -20,12 +19,12 @@ const c1 = keys.publicKey.encrypt(bn1);
 const c2 = keys.publicKey.encrypt(bn2);
 
 console.log('num1:', num1.toString());
-console.log('c1:', c1.toString(16));
+console.log('c1:', c1.toString(16), '\n');
 
 console.log('num2:', num2.toString());
 console.log('c2:', c2.toString(16), '\n');
 
-const encryptedSum = c1.mul(c2).mod(keys.publicKey.n2);
+const encryptedSum = c1.mul(c2).mod(keys.publicKey._n2);
 console.log('c1*c2:', encryptedSum.toString(16), '\n');
 
 const sum = keys.privateKey.decrypt(encryptedSum);
